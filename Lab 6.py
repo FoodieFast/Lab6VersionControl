7336138f5756158879cfe14ea58d06b13ed86db1
Ashidbilig Tamir <Ashidbilig.Tamir@gmail.com>
2023-10-31 23:24:34 -0400

    Added decode function

--- Lab 6.py
+++ Lab 6.py
@@ -6,6 +6,14 @@ def encode(password):
     return result
 # Goes through each digit of the password, adds 3, if greater 10, drops 10. Then added to result
 
+def decode(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        # decoding each digit of the password
+        decoded_password = decoded_password + decoded_digit
+    return decoded_password
+# returns the fully decoded password
 
 def main():
     while True:
@@ -21,12 +29,11 @@ def main():
         if user_input == "1":
             entered_password = input("Please enter your password to encode:")
             encoded_password = encode(entered_password)
-            print("Your password has been encoded and stored!")
-            print(encoded_password)
+            print("Your password has been encoded and stored!\n")
             # Encodes
 
         elif user_input == "2":
-            print(f"The encoded password is {encoded_password}, and the original password is {decode(encoded_password)}.")
+            print(f"The encoded password is {encoded_password}, and the original password is {decode(encoded_password)}.\n")
             # Decodes and prints output of both
 
         elif user_input == "3":
